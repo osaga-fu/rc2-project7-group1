a5fda95487044c11256af8375ae1887c0fe1302a
srb4by <oliversanchezcasiano@gmail.com>
2024-01-29 18:27:35 +0100

    conectar Front con Back

--- backend/src/main/java/group1/backend/controllers/ItemController.java
+++ backend/src/main/java/group1/backend/controllers/ItemController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import group1.backend.persistence.Item;
 import group1.backend.persistence.ItemRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ItemController {
 
     private ItemRepository repository;
