38599b8f12ba561a2acab211c372a7caad5dec1f
srb4by <oliversanchezcasiano@gmail.com>
2024-02-01 12:11:30 +0100

    add @DeleteMapping for id in controller.java

--- backend/src/main/java/group1/backend/controllers/ItemController.java
+++ backend/src/main/java/group1/backend/controllers/ItemController.java
@@ -2,13 +2,19 @@ package group1.backend.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import group1.backend.persistence.Item;
 import group1.backend.persistence.ItemRepository;
@@ -37,5 +43,16 @@ public class ItemController {
         Item item = new Item(request.getId(), request.getTitle(), request.getUrl());
         Item savedItem = repository.save(item);
         return new ItemResponse(savedItem.getId(), savedItem.getTitle(), savedItem.getUrl());
+    }
+
+    @DeleteMapping("/items/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteItemById(@PathVariable Long id) {
+        Optional<Item> optionalItem = repository.findById(id);
+        if (optionalItem.isPresent()) {
+            repository.delete(optionalItem.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not found");
+        }
     }
 }
