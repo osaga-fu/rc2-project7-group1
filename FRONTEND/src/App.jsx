d0e9a31e0656fe92a071c8816899cf978dc44c8b
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-02 20:03:14 +0100

    limpieza de imports innecesarios

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,9 +1,6 @@
 import "./App.css";
-import { FormAddItem } from "./components/FormAddItem/FormAddItem";
-import { Logo } from "./components/Logo/Logo";
 import { ResourceList } from "./components/ResourceList/ResourceList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { useState } from "react";
 import { Header } from "./components/Header/Header";
 
