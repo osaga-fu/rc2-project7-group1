77bd39f95f5bc0d7cd4d803ac446857c22446dd3
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-02 20:00:38 +0100

    refactorizado el react-router

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -5,6 +5,7 @@ import { ResourceList } from "./components/ResourceList/ResourceList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useState } from "react";
+import { Header } from "./components/Header/Header";
 
 function App() {
   const [needsReload, setNeedsReload] = useState(true);
@@ -12,23 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <div>
-            <header>
-              <Logo />
-              <FormAddItem setNeedsReload={setNeedsReload} />
-            </header>
-            <ResourceList setNeedsReload={setNeedsReload} needsReload={needsReload} />
-          </div>} />
-        <Route path="/edit/:id" element={
-          <div>
-            <header>
-              <Logo />
-              <FormAddItem setNeedsReload={setNeedsReload} />
-            </header>
-            <ResourceList setNeedsReload={setNeedsReload} needsReload={needsReload} />
-          </div>} />
+        <Route path="/" element={<Header setNeedsReload={setNeedsReload} />} />
+        <Route
+          path="/edit/:id"
+          element={<Header setNeedsReload={setNeedsReload} />}
+        />
       </Routes>
+      <ResourceList setNeedsReload={setNeedsReload} needsReload={needsReload} />
     </BrowserRouter>
   );
 }
